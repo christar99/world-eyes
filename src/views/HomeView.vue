7f790804911ca64a30fd4571728d4c8dbc7edf5e
이동아 <christar99@naver.com>
2022-06-12 17:59:14 +0900

    vue3 ts default setting

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="home">
+		asdasdasd
+	</div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+})
+export default class HomeView extends Vue { }
+</script>
